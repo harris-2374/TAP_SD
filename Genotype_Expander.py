e459017e630ffc9f5db363c3209dd29f841b9f5f
harris-2374 <41405364+harris-2374@users.noreply.github.com>
2018-09-03 19:48:31 -0400

    Rename and Clean up
    Shorten name and cleaned up code.

--- Genotype_Expander.py
+++ Genotype_Expander.py
@@ -2,12 +2,14 @@ import pandas as pd
 import argparse
 
 #----------------------------------------------------------------------------------------------------------------------#
+
 parser = argparse.ArgumentParser(description="Takes in vcf file and expands genotypes for further analysis.")
 parser.add_argument('-I', '--input', type=str, required=True, help='Please indicate the pathway to input vcf file.')
 parser.add_argument('-o', '--output', type=str, required=True, help="Indicate pathway to output locaiton, including "
                                                                     "what you would like to call the output.")
 parser.add_argument('-p', '--ploidy', type=int, required=True, help='Indicate the ploidy of the organism')
 args = parser.parse_args()
+
 
 #----------------------------------------------------------------------------------------------------------------------#
 
@@ -16,20 +18,14 @@ def mainfunction(vcf_file, output, ploidy):
 
     def header_fun():
         with open(vcf_file, mode='r', encoding='utf-8') as vcf_reader:
-
             header_len = 0
             header1 = []
-
-            # print(vcf_reader)
             try:
                 insert_flag = 0
                 for line in vcf_reader:
-                    # print(line)
                     if insert_flag == 2:
                         header1.append("##Genotype_Expander - Andrew Harris" + '\n')
                     if '##' in str(line):
-                        # print(line)
-                        # print("here")
                         header_len += 1
                         insert_flag += 1
                         header1.append(line)
@@ -38,17 +34,14 @@ def mainfunction(vcf_file, output, ploidy):
                         return header_len, header1
             except Exception:
                 vcf_reader.close()
-
                 return header_len, header1
 
     head_call = header_fun()
-    # print(head_call)
     header_length = head_call[0]
     header = head_call[1]
-
-    # print("Header length:", header_length)
-
+    
 #-----------------------------------------------------------------------------------------------------------------------
+
     def get_sample_amount(headlist):
         """
         :param headlist: VCF Headers and Sample names
@@ -58,7 +51,6 @@ def mainfunction(vcf_file, output, ploidy):
         for pos in headlist[9:]:
             temp = []
             temp.append(pos)
-
             for i6 in vcf_df[pos]:
                 tally = 0
                 for j in i6:
@@ -70,9 +62,6 @@ def mainfunction(vcf_file, output, ploidy):
                 temp.append(int(number_of_samps))
                 break
             sample_names.append(temp)
-        # print("Get_samp_amount Done")
-
-
         return sample_names
 
     def name_creator(samp_info):
@@ -89,27 +78,23 @@ def mainfunction(vcf_file, output, ploidy):
                 temp1 = str(i5[0]) + "_PseudoSample_" + str(j)
                 temp.append(temp1)
             names_fixed.append(temp)
-        # print("Name_creat Done")
         return names_fixed
 
     def genotype_handler(df_column, newnames, current, ploid):
         """
-
         :param df_column: Singular column of genotype data from a ONE sample
         :param newnames: list of new sample names
         :return: Dictionary of single sample genotype breakdown
         """
-        # print(df_column)
         dicts = []
         strt = 0
         stp = (ploid + 1)
-        for j in newnames:
+        for j2 in newnames:
             try:
-                if current in j:
-                    temp = [j]
+                if current in j2:
+                    temp = [j2]
                     for ii in df_column:
                         temp.append(str(ii[strt:stp]))
-
                     dicts.append(temp)
                 else:
                     continue
@@ -117,7 +102,6 @@ def mainfunction(vcf_file, output, ploidy):
                 continue
             strt = (stp + 1)
             stp += 4
-        # print("Genotype Handler Done")
         return dicts
 
     def genotype_dictionary_maker(df, names1, newnames, ploid):
@@ -134,26 +118,21 @@ def mainfunction(vcf_file, output, ploidy):
             for j in pos2:
                 se = pd.Series(j[1:])
                 vcf_df[j[0]] = se.values
-        # print("Genotype Dictionary maker done")
         return
-
+    
 #----------------------------------------------------------------------------------------------------------------------#
+
     header_temp = header
-    # print(header_temp)
     with open(output, 'w') as vcf:
         for iqq in header_temp:
             vcf.write(str(iqq))
-
     holder = str(vcf_file)
-
     df_read = pd.read_table(holder, skiprows=header_length, sep='\t', encoding="ISO-8859-1", low_memory=True)
-
-
     start = 0
     end = 1000
-    popp = 1
+    holder2 = 1
 
-    while popp > 0:
+    while holder2 > 0:
         try:
             vcf_df = pd.DataFrame(data=df_read[start:end])
 
@@ -172,9 +151,6 @@ def mainfunction(vcf_file, output, ploidy):
             vcf_df.to_csv(output, sep='\t', mode='a', encoding='utf-8', index=False)
             start = end + 1
             end += 1001
-            # print('Done With That One!')
-            # print(start)
-            # print(end)
         except:
             break
     return
